297a2e8db7d1a956a271718b9a60c404a22086c0
Karel Sluka <karel.sluka@gmail.com>
2024-11-25 12:01:47 +0100

    Finish interfaces and scss styles.

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -1,49 +1,59 @@
 export interface FormSchema {
-    type: string;
-    layout: Layout[];
-    widget: Widget;
-    properties: Properties;
-    required: string[];
-  }
-  
-  export interface Properties {
-    [key: string]: FormItem;
-  }
-  
-  export interface Layout {
-    type: string;
-    items: LayoutItem[];
-    config?: any;
-    widget: Widget;
-  }
-  
-  export interface LayoutItem {
-    type: string;
-    items: string[];
-    config?: any;
-    widget: Widget;
-  }
-  
-  export interface FormItem {
-    type: string;
-    title: string;
-    widget: Widget;
-    pattern?: string;
-    config?: any;
-    localizationData?: Object;
-    oneOf?: OneOf[];
-  }
-  
-  export interface Widget {
-    type: string;
-    validationMessages?: ValidationMessages;
-  }
-  
-  export interface ValidationMessages {
-    pattern?: string;
-  }
-  
-  export interface OneOf {
-    const: string;
-    title: string;
-  }
+  type: string;
+  layout: Layout[];
+  widget: Widget;
+  properties: Properties;
+  required: string[];
+}
+
+export interface Properties {
+  [key: string]: FormItem;
+}
+
+export interface Layout {
+  type: string;
+  items: LayoutItem[];
+  config?: Config;
+  widget: Widget;
+}
+
+export interface Config {
+  columns: Column[];
+  innerPageLayout?: boolean;
+}
+
+export interface Column {
+  width: number;
+  content: number[]
+}
+
+export interface LayoutItem {
+  type: string;
+  items: string[];
+  config?: Config;
+  widget: Widget;
+}
+
+export interface FormItem {
+  type: string;
+  title: string;
+  widget: Widget;
+  pattern?: string;
+  config?: any;
+  localizationData?: Object;
+  oneOf?: OneOf[];
+}
+
+export interface Widget {
+  type: string;
+  validationMessages?: ValidationMessages;
+}
+
+export interface ValidationMessages {
+  pattern?: string;
+}
+
+export interface OneOf {
+  const: string;
+  title: string;
+}
